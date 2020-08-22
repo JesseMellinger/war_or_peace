88cf1137fb1ec9294bd6c1c435c4eb31ddbb5345
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-08-22 14:27:27 -0600

    Update file path to Card, Deck, and Player classes

--- test/player_test.rb
+++ test/player_test.rb
@@ -1,8 +1,8 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require '../lib/card'
-require '../lib/deck'
-require '../lib/player'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
 
 class PlayerTest < Minitest::Test
 
