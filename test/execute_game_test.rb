9350474a8a5e604d4e73817929670143a7757227
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-08-22 13:51:51 -0600

    Add method to test class can check if player has lost

--- test/execute_game_test.rb
+++ test/execute_game_test.rb
@@ -65,4 +65,19 @@ class ExecuteGameTest < Minitest::Test
     assert game.respond_to?(:declare_winner)
   end
 
+  def test_it_can_check_if_player_has_lost
+    standard_deck = CardGenerator.new('lib/cards.txt').cards
+
+    deck1 = Deck.new(standard_deck.pop(26))
+    deck2 = Deck.new(standard_deck)
+
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    game = ExecuteGame.new(player1, player2)
+
+    assert game.player1.respond_to?(:has_lost?)
+    assert game.player2.respond_to?(:has_lost?)
+  end
+
 end
