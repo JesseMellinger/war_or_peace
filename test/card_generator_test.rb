fc1aec402ff5ac111b9df56b54e2ad871105eeee
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-08-22 16:18:01 -0600

    Add method to test for shuffled decks

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -12,7 +12,7 @@ class CardGeneratorTest < Minitest::Test
     assert_instance_of CardGenerator, standard_deck
   end
 
-  def test_get_shuffled_deck
+  def test_it_can_generate_a_deck_cards
     filename = './lib/cards.txt'
     standard_deck = CardGenerator.new(filename)
 
@@ -39,6 +39,17 @@ class CardGeneratorTest < Minitest::Test
     standard_deck = CardGenerator.new(filename)
 
     assert standard_deck.cards.all? {|card| card.respond_to?(:rank)}
+  end
+
+  def test_it_can_generate_a_shuffled_deck
+    filename = './lib/cards.txt'
+    standard_deck1 = CardGenerator.new(filename)
+    standard_deck2 = CardGenerator.new(filename)
+
+    assert standard_deck1.cards.zip(standard_deck2.cards).any? { |cards|
+      cards[0].suit != cards[1].suit && cards[0].rank != cards[1].rank &&
+      cards[0].value != cards[1].value
+    }
   end
 
 end
