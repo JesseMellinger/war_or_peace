09308bdae154af86ec1a255094a94be16c47ed8f
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-08-22 14:20:57 -0600

    Update file paths to CardGenerator class and card.txt

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -1,19 +1,19 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require '../lib/card_generator'
+require './lib/card_generator'
 
 
 class CardGeneratorTest < Minitest::Test
 
   def test_card_generator_exists
-    filename = 'cards.txt'
+    filename = './lib/cards.txt'
     standard_deck = CardGenerator.new(filename)
 
     assert_instance_of CardGenerator, standard_deck
   end
 
   def test_get_shuffled_deck
-    filename = '../lib/cards.txt'
+    filename = './lib/cards.txt'
     standard_deck = CardGenerator.new(filename)
 
     assert_equal 52, standard_deck.cards.count
